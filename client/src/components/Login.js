3a3241292a1e68c9f03bcbca71794bbe2aeb3b3a
dhirajbantawarai <61051104+dhirajbantawarai@users.noreply.github.com>
2024-01-03 22:27:58 +0545

    some changes

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import "../styles/login.css";
 import { useProductContext } from "../context/StoreContext";
@@ -11,38 +10,42 @@ export const Login = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     // Handle login logic here
-    loginUser(username, password)
+    loginUser(username, password);
   };
 
   return (
     <>
       <div className="login-page">
-        <h2>Login</h2>
-        <br></br>
-        <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit">Login</button>
-          <a href="*" className="forget-password">
-            Forget password?
-          </a>
-        </form>
+        <div className="loginbox">
+          <h2>Login</h2>
+          <br></br>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+            <a href="#" className="forget-password">
+              Forget password?
+            </a>
+          </form>
+        </div>
       </div>
     </>
   );
